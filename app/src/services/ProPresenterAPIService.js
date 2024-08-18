05a6ae844cb2b342fa9eecbc28fb3538e5a6aa2f
Andrew DM <razor@email.com>
2024-08-18 10:22:15 +0300

    Update apiUrl

--- app/src/services/ProPresenterAPIService.js
+++ app/src/services/ProPresenterAPIService.js
@@ -1,5 +1,5 @@
-const apiUrl = 'http://localhost:3001'; // DEBUG
-//const apiUrl = ''; // TEST | RELEASE
+//const apiUrl = 'http://localhost:3001'; // DEBUG
+const apiUrl = ''; // TEST | RELEASE
 
 export async function getPlaylists() {
   return await fetch(`${apiUrl}/playlists`, { method: 'GET' }).then(res => res.json());
