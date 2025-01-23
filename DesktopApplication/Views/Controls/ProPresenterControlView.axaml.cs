b476295fef579c8e44f5c5b664dd474424aedf56
Andrii Demianchuk <andrew.nc.dm@gmail.com>
2025-01-23 13:42:00 +0200

    Arrange UI componentes on a window.

---
+++ DesktopApplication/Views/Controls/ProPresenterControlView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace ProPresenter7WEB.DesktopApplication.Views.Controls;
+
+public partial class ProPresenterControlView : UserControl
+{
+    public ProPresenterControlView()
+    {
+        InitializeComponent();
+    }
+}
