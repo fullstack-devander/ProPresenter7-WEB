04a6a21f4bb6f381af821a29d35f9ec692fcd389
Andrii Demianchuk <andrew.nc.dm@gmail.com>
2025-01-26 11:27:04 +0200

    Install MessageBox pakege for Avalonia framework

--- DesktopApplication/ViewModels/Controls/ProPresenterControlViewModel.cs
+++ DesktopApplication/ViewModels/Controls/ProPresenterControlViewModel.cs
@@ -1,4 +1,8 @@
-﻿using ProPresenter7WEB.Service;
+﻿using MsBox.Avalonia;
+using MsBox.Avalonia.Enums;
+using ProPresenter7WEB.Service;
+using System;
+using System.Threading.Tasks;
 
 namespace ProPresenter7WEB.DesktopApplication.ViewModels.Controls
 {
@@ -17,8 +21,34 @@ namespace ProPresenter7WEB.DesktopApplication.ViewModels.Controls
 
         public async void Connect()
         {
-            _proPresenterService.SetProPresenterConnection(IpAddress, int.Parse(Port));
-            var response = await _proPresenterService.GetProPresenterInfoAsync();
+            try
+            {
+                if (string.IsNullOrEmpty(IpAddress))
+                {
+                    await ShowFailedConnectionMessageBoxAsync("'Ip Address' cannot be empty.");
+                    return;
+                }
+
+                if (string.IsNullOrEmpty(Port))
+                {
+                    await ShowFailedConnectionMessageBoxAsync("'Port' cannot be empty.");
+                    return;
+                }
+
+                _proPresenterService.SetProPresenterConnection(IpAddress, int.Parse(Port));
+                var response = await _proPresenterService.GetProPresenterInfoAsync();
+            }
+            catch (Exception ex)
+            {
+                await ShowFailedConnectionMessageBoxAsync(ex.Message);
+            }
+        }
+
+        private async static Task ShowFailedConnectionMessageBoxAsync(string message)
+        {
+            await MessageBoxManager
+                .GetMessageBoxStandard($"Failed Connection", message, ButtonEnum.Ok)
+                .ShowAsync();
         }
     }
 }
